dd3f84605ded444535877f4f16cca20e0d03b59a
petermcaup <pdmca7@gmail.com>
2025-11-14 11:31:52 -0500

    added lpf

--- Final_Project_Code_Base/Final_Project_Code_Base.ino
+++ Final_Project_Code_Base/Final_Project_Code_Base.ino
@@ -119,16 +119,15 @@ void loop()
   // ******************************************************************
   //  Compute the output of the filter using the cascaded SOS sections
    yv = IIR_Generic(xv); // second order systems cascade  
-
-
-
+   
+   yLF = IIR_LPF(xv); // Low Pass Filter
 
   //  Compute the latest output of the running stats for the output of the filters.
   //  Pass the entire set of output values, the latest stats structure and the reset flag
 
   
-  //  statsReset = (statsLF.tick%100 == 0);
-  //  getStats( yv, statsLF, statsReset);
+  statsReset = (statsLF.tick%100 == 0);
+  getStats( yLF, statsLF, statsReset);
   //  stdLF = statsLF.stdev;
 
 
@@ -278,6 +277,77 @@ static float a[numStages][3];
 G[0] = 0.0095258;
 b[0][0] = 1.0000000; b[0][1] = 2.0000000; b[0][2]= 1.0000000;
 a[0][0] = 1.0000000; a[0][1] =  -1.7055521; a[0][2] =  0.7436552;
+
+//  **** Stop copying MATLAB code here  ****
+
+
+
+  //  Iterate over each second order stage.  For each stage shift the input data
+  //  buffer ( x[kk] ) by one and the output data buffer by one ( y[k] ).  Then bring in 
+  //  a new sample xv into the buffer;
+  //
+  //  Then execute the recusive filter on the buffer
+  //
+  //  y[k] = -a[2]*y[k-2] + -a[1]*y[k-1] + g*b[0]*x[k] + b[1]*x[k-1] + b[2]*x[k-2] 
+  //
+  //  Pass the output from this stage to the next stage by setting the input
+  //  variable to the next stage x to the output of the current stage y
+  //  
+  //  Repeat this for each second order stage of the filter
+
+  
+  for (i =0; i<numStages; i++)
+    {
+      yM2[i] = yM1[i]; yM1[i] = yM0[i];  xM2[i] = xM1[i]; xM1[i] = xM0[i], xM0[i] = G[i]*xv;
+      yv = -a[i][2]*yM2[i] - a[i][1]*yM1[i] + b[i][2]*xM2[i] + b[i][1]*xM1[i] + b[i][0]*xM0[i];
+      yM0[i] = yv;
+      xv = yv;
+    }
+//
+//  execUsec += micros()-startTime;
+  
+  return yv;
+}
+
+float IIR_LPF(float xv)
+{
+
+  //  ***  Copy variable declarations from MATLAB generator to here  ****
+
+    //Filter specific variable declarations
+    const int numStages = 4;
+    static float G[numStages];
+    static float b[numStages][3];
+    static float a[numStages][3];
+
+//  *** Stop copying MATLAB variable declarations here
+  
+  int stage;
+  int i;
+  static float xM0[numStages] = {0.0}, xM1[numStages] = {0.0}, xM2[numStages] = {0.0};
+  static float yM0[numStages] = {0.0}, yM1[numStages] = {0.0}, yM2[numStages] = {0.0};
+  
+  float yv = 0.0;
+  unsigned long startTime;
+
+
+
+//  ***  Copy variable initialization code from MATLAB generator to here  ****
+
+    //BWRTH low, order 7, 14 BPM
+
+    G[0] = 0.0095395;
+    b[0][0] = 1.0000000; b[0][1] = 0.9921697; b[0][2]= 0.0000000;
+    a[0][0] = 1.0000000; a[0][1] =  -0.8631768; a[0][2] =  0.0000000;
+    G[1] = 0.0095395;
+    b[1][0] = 1.0000000; b[1][1] = 2.0143760; b[1][2]= 1.0144400;
+    a[1][0] = 1.0000000; a[1][1] =  -1.7484237; a[1][2] =  0.7673836;
+    G[2] = 0.0095395;
+    b[2][0] = 1.0000000; b[2][1] = 2.0033680; b[2][2]= 1.0034310;
+    a[2][0] = 1.0000000; a[2][1] =  -1.8133797; a[2][2] =  0.8330440;
+    G[3] = 0.0095395;
+    b[3][0] = 1.0000000; b[3][1] = 1.9900862; b[3][2]= 0.9901480;
+    a[3][0] = 1.0000000; a[3][1] =  -1.9162539; a[3][2] =  0.9370338;
 
 //  **** Stop copying MATLAB code here  ****
 
