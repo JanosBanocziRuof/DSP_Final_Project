21c183ec2e6cc64aee739ec7b69468fc160ae312
JanosBanocziRuof <67029836+JanosBanocziRuof@users.noreply.github.com>
2025-11-14 11:32:34 -0500

    Added HFP and called in loop.

--- Final_Project_Code_Base/Final_Project_Code_Base.ino
+++ Final_Project_Code_Base/Final_Project_Code_Base.ino
@@ -118,7 +118,7 @@ void loop()
 
   // ******************************************************************
   //  Compute the output of the filter using the cascaded SOS sections
-   yv = IIR_Generic(xv); // second order systems cascade  
+   yMF = IIR_BPF(xv); // 7th order BPF
 
 
 
@@ -127,9 +127,9 @@ void loop()
   //  Pass the entire set of output values, the latest stats structure and the reset flag
 
   
-  //  statsReset = (statsLF.tick%100 == 0);
-  //  getStats( yv, statsLF, statsReset);
-  //  stdLF = statsLF.stdev;
+  statsReset = (statsMF.tick%100 == 0);
+  getStats( yMF, statsMF, statsReset);
+  stdMF = statsMF.stdev;
 
 
   //*******************************************************************
@@ -280,6 +280,85 @@ b[0][0] = 1.0000000; b[0][1] = 2.0000000; b[0][2]= 1.0000000;
 a[0][0] = 1.0000000; a[0][1] =  -1.7055521; a[0][2] =  0.7436552;
 
 //  **** Stop copying MATLAB code here  ****
+
+
+
+  //  Iterate over each second order stage.  For each stage shift the input data
+  //  buffer ( x[kk] ) by one and the output data buffer by one ( y[k] ).  Then bring in 
+  //  a new sample xv into the buffer;
+  //
+  //  Then execute the recusive filter on the buffer
+  //
+  //  y[k] = -a[2]*y[k-2] + -a[1]*y[k-1] + g*b[0]*x[k] + b[1]*x[k-1] + b[2]*x[k-2] 
+  //
+  //  Pass the output from this stage to the next stage by setting the input
+  //  variable to the next stage x to the output of the current stage y
+  //  
+  //  Repeat this for each second order stage of the filter
+
+  
+  for (i =0; i<numStages; i++)
+    {
+      yM2[i] = yM1[i]; yM1[i] = yM0[i];  xM2[i] = xM1[i]; xM1[i] = xM0[i], xM0[i] = G[i]*xv;
+      yv = -a[i][2]*yM2[i] - a[i][1]*yM1[i] + b[i][2]*xM2[i] + b[i][1]*xM1[i] + b[i][0]*xM0[i];
+      yM0[i] = yv;
+      xv = yv;
+    }
+//
+//  execUsec += micros()-startTime;
+  
+  return yv;
+}
+
+float IIR_BPF(float xv)
+{
+  //  ***  Copy variable declarations from MATLAB generator to here  ****
+
+  //Filter specific variable declarations
+  const int numStages = 7;
+  static float G[numStages];
+  static float b[numStages][3];
+  static float a[numStages][3];
+
+//  *** Stop copying MATLAB variable declarations here
+  
+  int stage;
+  int i;
+  static float xM0[numStages] = {0.0}, xM1[numStages] = {0.0}, xM2[numStages] = {0.0};
+  static float yM0[numStages] = {0.0}, yM1[numStages] = {0.0}, yM2[numStages] = {0.0};
+  
+  float yv = 0.0;
+  unsigned long startTime;
+
+
+
+  //  ***  Copy variable initialization code from MATLAB generator to here  ****
+  
+  //BWRTH bandpass, order 7, [14 40] BPM
+  
+  G[0] = 0.1254753;
+  b[0][0] = 1.0000000; b[0][1] = 0.0003589; b[0][2]= -0.9901062;
+  a[0][0] = 1.0000000; a[0][1] =  -1.6415097; a[0][2] =  0.7323348;
+  G[1] = 0.1254753;
+  b[1][0] = 1.0000000; b[1][1] = 2.0086252; b[1][2]= 1.0086481;
+  a[1][0] = 1.0000000; a[1][1] =  -1.7051100; a[1][2] =  0.7592171;
+  G[2] = 0.1254753;
+  b[2][0] = 1.0000000; b[2][1] = 2.0021219; b[2][2]= 1.0021447;
+  a[2][0] = 1.0000000; a[2][1] =  -1.6575316; a[2][2] =  0.7882426;
+  G[3] = 0.1254753;
+  b[3][0] = 1.0000000; b[3][1] = 1.9940328; b[3][2]= 0.9940556;
+  a[3][0] = 1.0000000; a[3][1] =  -1.7990429; a[3][2] =  0.8318872;
+  G[4] = 0.1254753;
+  b[4][0] = 1.0000000; b[4][1] = -2.0092863; b[4][2]= 1.0093129;
+  a[4][0] = 1.0000000; a[4][1] =  -1.8811605; a[4][2] =  0.9055428;
+  G[5] = 0.1254753;
+  b[5][0] = 1.0000000; b[5][1] = -1.9935774; b[5][2]= 0.9936038;
+  a[5][0] = 1.0000000; a[5][1] =  -1.7532897; a[5][2] =  0.9159845;
+  G[6] = 0.1254753;
+  b[6][0] = 1.0000000; b[6][1] = -2.0022749; b[6][2]= 1.0023015;
+  a[6][0] = 1.0000000; a[6][1] =  -1.9465589; a[6][2] =  0.9680273;
+  
+  //  **** Stop copying MATLAB code here  ****
 
 
 
