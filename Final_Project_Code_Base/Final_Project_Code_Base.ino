4975cf67dfc134d7003c0f035a33b45825616e2b
cdl9445 <98424029+cdl9445@users.noreply.github.com>
2025-11-14 16:31:11 +0000

    Added HPF

--- Final_Project_Code_Base/Final_Project_Code_Base.ino
+++ Final_Project_Code_Base/Final_Project_Code_Base.ino
@@ -118,7 +118,7 @@ void loop()
 
   // ******************************************************************
   //  Compute the output of the filter using the cascaded SOS sections
-   yv = IIR_Generic(xv); // second order systems cascade  
+   yHF = IIR_HPF(xv); // second order systems cascade  
 
 
 
@@ -127,9 +127,9 @@ void loop()
   //  Pass the entire set of output values, the latest stats structure and the reset flag
 
   
-  //  statsReset = (statsLF.tick%100 == 0);
-  //  getStats( yv, statsLF, statsReset);
-  //  stdLF = statsLF.stdev;
+    statsReset = (statsHF.tick%100 == 0);
+    getStats( yHF, statsHF, statsReset);
+    stdHF = statsHF.stdev;
 
 
   //*******************************************************************
@@ -155,13 +155,13 @@ void loop()
    printArray[0] = loopTick;  //  The sample number -- always print this
    printArray[1] = xv;        //  Column 2
    
-//   printArray[2] = yLF;       //  Column 3
-//   printArray[3] = yMF;       //  Column 4, etc...
-//   printArray[4] = yHF;
-//   printArray[5] = stdLF;
-//   printArray[6] = stdMF;
-//   printArray[7] = stdHF;
-//   printArray[8] = float(alarmCode);
+   printArray[2] = yLF;       //  Column 3
+  printArray[3] = yMF;       //  Column 4, etc...
+   printArray[4] = yHF;
+   printArray[5] = stdLF;
+   printArray[6] = stdMF;
+  printArray[7] = stdHF;
+  printArray[8] = float(alarmCode);
 
    numValues = 2;  // The number of columns to be sent to the serial monitor (or MATLAB)
 
@@ -247,6 +247,8 @@ int FIR_Generic(long inputX, int sampleNumber)
 }
 
 
+
+
 //*******************************************************************************
 float IIR_Generic(float xv)
 {  
@@ -278,6 +280,79 @@ static float a[numStages][3];
 G[0] = 0.0095258;
 b[0][0] = 1.0000000; b[0][1] = 2.0000000; b[0][2]= 1.0000000;
 a[0][0] = 1.0000000; a[0][1] =  -1.7055521; a[0][2] =  0.7436552;
+
+//  **** Stop copying MATLAB code here  ****
+
+
+
+  //  Iterate over each second order stage.  For each stage shift the input data
+  //  buffer ( x[kk] ) by one and the output data buffer by one ( y[k] ).  Then bring in 
+  //  a new sample xv into the buffer;
+  //
+  //  Then execute the recusive filter on the buffer
+  //
+  //  y[k] = -a[2]*y[k-2] + -a[1]*y[k-1] + g*b[0]*x[k] + b[1]*x[k-1] + b[2]*x[k-2] 
+  //
+  //  Pass the output from this stage to the next stage by setting the input
+  //  variable to the next stage x to the output of the current stage y
+  //  
+  //  Repeat this for each second order stage of the filter
+
+  
+  for (i =0; i<numStages; i++)
+    {
+      yM2[i] = yM1[i]; yM1[i] = yM0[i];  xM2[i] = xM1[i]; xM1[i] = xM0[i], xM0[i] = G[i]*xv;
+      yv = -a[i][2]*yM2[i] - a[i][1]*yM1[i] + b[i][2]*xM2[i] + b[i][1]*xM1[i] + b[i][0]*xM0[i];
+      yM0[i] = yv;
+      xv = yv;
+    }
+//
+//  execUsec += micros()-startTime;
+  
+  return yv;
+}
+
+float IIR_HPF(float xv)
+{  
+  // 5th Order HPF with a 38 BPM round off frequency 
+
+  //  ***  Copy variable declarations from MATLAB generator to here  ****
+
+//Filter specific variable declarations
+const int numStages = 4;
+static float G[numStages];
+static float b[numStages][3];
+static float a[numStages][3];
+
+//  *** Stop copying MATLAB variable declarations here
+  
+  int stage;
+  int i;
+  static float xM0[numStages] = {0.0}, xM1[numStages] = {0.0}, xM2[numStages] = {0.0};
+  static float yM0[numStages] = {0.0}, yM1[numStages] = {0.0}, yM2[numStages] = {0.0};
+  
+  float yv = 0.0;
+  unsigned long startTime;
+
+
+
+//  ***  Copy variable initialization code from MATLAB generator to here  ****
+
+
+//BWRTH high, order 7, 38 BPM
+
+G[0] = 0.7981509;
+b[0][0] = 1.0000000; b[0][1] = -0.9927070; b[0][2]= 0.0000000;
+a[0][0] = 1.0000000; a[0][1] =  -0.6643984; a[0][2] =  0.0000000;
+G[1] = 0.7981509;
+b[1][0] = 1.0000000; b[1][1] = -2.0134915; b[1][2]= 1.0135479;
+a[1][0] = 1.0000000; a[1][1] =  -1.3665943; a[1][2] =  0.4824264;
+G[2] = 0.7981509;
+b[2][0] = 1.0000000; b[2][1] = -2.0030945; b[2][2]= 1.0031498;
+a[2][0] = 1.0000000; a[2][1] =  -1.4849456; a[2][2] =  0.6108092;
+G[3] = 0.7981509;
+b[3][0] = 1.0000000; b[3][1] = -1.9907069; b[3][2]= 0.9907608;
+a[3][0] = 1.0000000; a[3][1] =  -1.6973622; a[3][2] =  0.8412301;
 
 //  **** Stop copying MATLAB code here  ****
 
